f738069c22fa187a1c0d00455ee1dc6c8c277205
Taisuke Fukuno <fukuno@jig.jp>
2021-03-30 06:56:34 +0900

    add bbs code

---
+++ bbs.js
@@ -0,0 +1,19 @@
+import { Server } from "https://js.sabae.cc/Server.js";
+import { jsonfs } from "https://js.sabae.cc/jsonfs.js";
+
+const datafn = "data.json";
+const data = jsonfs.read(datafn) || [];
+
+class MyServer extends Server {
+  api(path, req) {
+    if (path == "/api/list") {
+      return data;
+    } else if (path == "/api/add") {
+      data.push(req);
+      jsonfs.write(datafn, data);
+      return "ok";
+    }
+  }
+}
+
+new MyServer(8008);
